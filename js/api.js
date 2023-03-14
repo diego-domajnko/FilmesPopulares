376b155f5b4b94e08450a57cc22e1b8ef228b743
diego-domajnko <diegodomajnko@gmail.com>
2023-03-14 16:15:44 -0300

    update

--- js/api.js
+++ js/api.js
@@ -1,9 +1,1 @@
-// export const listaFilmesPopulares = filmesPopulares();
-
-// async function filmesPopulares() {
-//   const listaFilmes = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=003451298b974b48b7de3dd3aab4a07b&language=en-US&page=1');
-//   const converteListaFilmes = await listaFilmes.json();
-//   return converteListaFilmes;
-// }
-
 export const apiKey = "003451298b974b48b7de3dd3aab4a07b";
